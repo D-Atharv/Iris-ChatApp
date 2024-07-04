cf717543a96e95f31f0deb8a500b5cff83df230e
Atharv Dewangan <127681266+D-Atharv@users.noreply.github.com>
2024-07-05 00:04:14 +0530

    Update page.tsx
    updating search_bar(left) to be sticky

--- app/people/page.tsx
+++ app/people/page.tsx
@@ -41,6 +41,43 @@
 // export default App
 
 
+// import React from 'react'
+// import Header from './components/Header'
+// import Left from './components/Left'
+// import RightTop from './components/RightTop'
+// import LeftName from './components/LeftName'
+// import Bottom from './components/LeftBottom'
+// import RightBottom from './components/RightBottom'
+// import RightMiddle from './components/RightMiddle'
+
+// const App = () => {
+//     return (
+//         <>
+//             <div className='grid min-h-screen w-screen gap-y-0 bg-c_line_gray'>
+//                 <div className='sticky top-0 w-screen z-10'><Header /></div>
+
+//                 <div className='grid lg:grid-cols-12 row-span-12'>
+//                     <div className='col-span-4 grid grid-rows-10 xl:border-r-4 border-c_black'>
+//                         <div className='row-span-1'><Left /></div>
+//                         <div className='row-span-7'><LeftName /></div>
+//                         <div className='sticky bottom-0 row-span-2'><Bottom /></div>
+//                     </div>
+
+//                     <div className='hidden lg:grid grid-rows-11 col-span-8'>
+//                         <div className='row-span-1'><RightTop /></div>
+//                         <div className='row-span-9'><RightMiddle /></div>
+//                         <div className='row-span-1'><RightBottom /></div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default App
+
+
+
 import React from 'react'
 import Header from './components/Header'
 import Left from './components/Left'
@@ -58,7 +95,7 @@ const App = () => {
 
                 <div className='grid lg:grid-cols-12 row-span-12'>
                     <div className='col-span-4 grid grid-rows-10 xl:border-r-4 border-c_black'>
-                        <div className='row-span-1'><Left /></div>
+                        <div className='sticky top-0 row-span-1'><Left /></div>
                         <div className='row-span-7'><LeftName /></div>
                         <div className='sticky bottom-0 row-span-2'><Bottom /></div>
                     </div>
@@ -75,3 +112,4 @@ const App = () => {
 }
 
 export default App
+
